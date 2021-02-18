c81f39b59f4bb88d920f8908397390c3891cdd8d
akankshacg <akanksha.b.akanksha@capgemini.com>
2021-02-18 20:58:46 +0530

    1 file modified, 27 added

---
+++ src/app/work-experience/work-experience.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-work-experience',
+  templateUrl: './work-experience.component.html',
+  styleUrls: ['./work-experience.component.scss']
+})
+export class WorkExperienceComponent implements OnInit {
+
+  constructor() { }
+  fieldArray: Array<any> = [];
+  newAttribute: any = {}; 
+  addFieldValue(index) {
+    if (this.fieldArray.length <= 2) {
+      this.fieldArray.push(this.newAttribute);
+      this.newAttribute = {};
+    } else {
+
+    }
+  }
+  ngOnInit(): void {
+  }
+
+}
